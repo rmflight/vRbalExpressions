f9cc62b60548d96f89aeac683887d4473dd74fa0
Robert Flight <r.flight79@gmail.com>
2013-08-26 15:35:11 -0400

    changed tests to use grepl and expect_true or expect_false

--- inst/tests/vRbalExpression_tests.R
+++ inst/tests/vRbalExpression_tests.R
@@ -4,116 +4,116 @@ test_that("something works", {
   useExpr <- verbalExpression(something=NULL)
   testString <- ""
   
-  expect_that(-1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_false(grepl(useExpr, testString))
   
   testString <- "a"
-  expect_that(1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_true(grepl(useExpr, testString))
 })
 
 test_that("anything works", {
   useExpr <- verbalExpression(startofline=NULL, anything=NULL)
   testString <- "what"
-  expect_that(1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_true(grepl(useExpr, testString))
 })
 
 test_that("anythingBut works", {
   useExpr <- verbalExpression(startofline=NULL, anythingBut="w")
   testString <- "what"
-  expect_that(1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_true(grepl(useExpr, testString))
 })
 
 test_that("somethingBut works", {
   useExpr <- verbalExpression(somethingBut="a")
   testString <- ""
-  expect_that(-1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_false(grepl(useExpr, testString))
   testString <- "b"
-  expect_that(1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_true(grepl(useExpr, testString))
   testString <- "a"
-  expect_that(-1, is_equivalent_to(regexpr(useExpr, testString)))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("startofline works", {
   useExpr <- verbalExpression(startofline=NULL, then="a")
   testString <- "a"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "ba"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("endofline works", {
   useExpr <- verbalExpression(find="a", endofline=NULL)
   testString <- "a"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "ab"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
   
   useExpr <- verbalExpression(endofline="a")
   testString <- "a"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "ab"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("maybe works", {
   useExpr <- verbalExpression(startofline=NULL, then="a", maybe="b")
   
   testString <- "acb"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "abc"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
 })
 
 test_that("anyOf works", {
   useExpr <- verbalExpression(startofline=NULL, then="a", anyOf="xyz")
   testString <- "ay"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "abc"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("or works", {
   useExpr <- verbalExpression(startofline=NULL, then="abc", or="def")
   
   testString <- "defzzz"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   testString <- "abczzz"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "xyzabc"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("linebreak works", {
   useExpr <- verbalExpression(startofline=NULL, then="abc", linebreak=NULL, then="def")
   
   testString <- "abc\r\ndef"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "abc\r\n def"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("br works", {
   useExpr <- verbalExpression(startofline=NULL, then="abc", br=NULL, then="def")
   testString <- "abc\r\ndef"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "abc\r\n def"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
 test_that("tab works", {
   useExpr <- verbalExpression(startofline=NULL, tab=NULL, then="abc")
   
   testString <- "\tabc"
-  expect_equivalent(1, regexpr(useExpr, testString))
+  expect_true(grepl(useExpr, testString))
   
   testString <- "abc"
-  expect_equivalent(-1, regexpr(useExpr, testString))
+  expect_false(grepl(useExpr, testString))
 })
 
